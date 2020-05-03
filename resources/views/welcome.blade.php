8ae380b61daf732e5972f165fe3b977f87640877
kay siebel <kay_siebel@gmx.de>
2020-05-03 17:14:40 +0200

    First experiments with elements and styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,422 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<body>
+    <!-- <section class="page-content"> -->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <section class="header-section">
+        <div class="header flex">i!</div>
+    </section>
 
-            .full-height {
-                height: 100vh;
-            }
+    <section class="main-container">
+        <div class="flex flex-wrap">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="card margin-16 border-radius shadow">
+                <img class="border-bottom-fat" src="https://www.citizencider.com/wp-content/uploads/2019/01/placeholder.jpg" alt="...">
+                <div class="card-content">
+                    <p class="card-text padding-8 margin-8">
+                        Lorem
+                    </p>
                 </div>
             </div>
+
+
+            <div class="card margin-16 border-radius shadow">
+                <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320185/classicstaremblem-1682.png" alt="...">
+                <div class="card-content">
+
+                    <p class="card-text padding-8 margin-8">
+                        "Artikel-Name"</p>
+
+                    <button class="accordion ripple border-bottom padding-4">Überblick</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Bestand: 1000 Stück</p>
+                        <p class="">
+                            Lager-Ort: Halle 3</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Gewicht</p>
+                        <p class="">
+                            Mindest-Bestand</p>
+                        <p class="">
+                            Bestell-Bestand</p>
+                        <p class="">
+                            Höchst-Bestand</p>
+                        <p class="">
+                            Gebinde-Größe</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Einheiten/Stück</p>
+                        <p class="">
+                            Bestand</p>
+                        <p class="">
+                            Gewicht</p>
+                        <p class="">
+                            Mindest-Bestand</p>
+                        <p class="">
+                            Bestell-Bestand</p>
+                        <p class="">
+                            Höchst-Bestand</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Lagerort</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Max-Gewicht</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">i!</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Gesamt-Gewicht/Stück</p>
+                        <p class="">
+                            Gesamt-Gewicht/Gebinde</p>
+                        <p class="">
+                            aktuelles Gewicht/Lagerort</p>
+                        <p class="">
+                            Letzter Zugang</p>
+                        <p class="">
+                            Letzter Abgang</p>
+                        <p class="">
+                            Durchschnittsbestand</p>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="card margin-16 border-radius shadow">
+                <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320195/tropicalhouse-1831.png" alt="...">
+                <div class="card-content">
+
+                    <p class="card-text padding-8 margin-8">
+                        "Artikel-Name"</p>
+
+                    <button class="accordion ripple border-bottom padding-4">Überblick</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Bestand: 1000 Stück</p>
+                        <p class="">
+                            Lager-Ort: Halle 3</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Gewicht</p>
+                        <p class="">
+                            Mindest-Bestand</p>
+                        <p class="">
+                            Bestell-Bestand</p>
+                        <p class="">
+                            Höchst-Bestand</p>
+                        <p class="">
+                            Gebinde-Größe</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Einheiten/Stück</p>
+                        <p class="">
+                            Bestand</p>
+                        <p class="">
+                            Gewicht</p>
+                        <p class="">
+                            Mindest-Bestand</p>
+                        <p class="">
+                            Bestell-Bestand</p>
+                        <p class="">
+                            Höchst-Bestand</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">Lagerort</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Max-Gewicht</p>
+                    </div>
+
+                    <button class="accordion ripple border-bottom padding-4">i!</button>
+                    <div class="accordion-panel border-bottom">
+                        <p class="">
+                            Gesamt-Gewicht/Stück</p>
+                        <p class="">
+                            Gesamt-Gewicht/Gebinde</p>
+                        <p class="">
+                            aktuelles Gewicht/Lagerort</p>
+                        <p class="">
+                            Letzter Zugang</p>
+                        <p class="">
+                            Letzter Abgang</p>
+                        <p class="">
+                            Durchschnittsbestand</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- <div>
+            <button class="ripple">Button</button>
+        </div> -->
+
         </div>
-    </body>
+    </section>
+
+    <section class="footer-section">
+        <div class="footer">
+            <a href="#index">
+
+                <img class="footer-item" src="https://image.flaticon.com/icons/svg/77/77196.svg">
+                <a href="#index">
+                    <img class="footer-item" src="https://image.flaticon.com/icons/svg/77/77173.svg">
+                    <a href="#index">
+                        <img class="footer-item" src="https://image.flaticon.com/icons/svg/77/77139.svg">
+                        <a href="#index">
+                            <img class="footer-item" src="https://image.flaticon.com/icons/svg/77/77143.svg">
+                            <a href="#index">
+                                <img class="footer-item" src="https://image.flaticon.com/icons/svg/77/77196.svg">
+    </section>
+    <!-- ==================================== -->
+    <script>
+        var accordion = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < accordion.length; i++) {
+
+            accordion[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+            });
+        }
+    </script>
+    <!-- ==================================== -->
+    <style>
+        body {
+            margin: 0;
+            top: 0;
+            background-color: var(--darkest);
+        }
+
+        /* .page-content {
+            background-color: var(--darkest);
+            margin: 0;
+            top: 0;
+            filter: hue-rotate(180deg);
+        } */
+
+        /* ================= experimente ================= */
+        .accordion {
+            background-color: var(--dark);
+            color: var(--light);
+            cursor: pointer;
+            width: 100%;
+            border: none;
+            text-align: center;
+            outline: none;
+            font-size: 15px;
+        }
+
+        .accordion-panel {
+            background-color: var(--light);
+            color: var(--darkest);
+            padding: 0 18px;
+            display: none;
+            overflow: hidden;
+        }
+
+        .ripple {
+            background-position: center;
+            transition: background .5s;
+        }
+
+        .ripple:hover {
+            background: var(--light) radial-gradient(circle, transparent 1%, var(--dark) 10%) center/15000%;
+            color: var(--darkest);
+        }
+
+        .ripple:active {
+            background-color: var(--lightly);
+            background-size: 100%;
+            transition: background 0s;
+        }
+
+        /* ================= experimente ================= */
+
+        .header-section {
+            position: fixed;
+            top: 0;
+            width: 100vw;
+            background: var(--darkest);
+        }
+
+        .header {
+            margin: 8px;
+            margin-top: 2px;
+            height: 40px;
+            background: linear-gradient(to right, var(--darkest), var(--light) 10%, var(--light) 90%, var(--darkest));
+            color: var(--darkest);
+            text-align: center;
+            font-size: 2em;
+            border-radius: 12px;
+        }
+
+        .footer-section {
+            position: fixed;
+            bottom: 0;
+            width: 100vw;
+            background: var(--darkest);
+        }
+
+        .footer {
+            margin: 8px;
+            margin-bottom: 2px;
+            height: 40px;
+            background: linear-gradient(to right, var(--darkest), var(--light) 10%, var(--light) 90%, var(--darkest));
+            color: var(--darkest);
+            border-radius: 12px;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+
+        }
+
+        .footer-item {
+            text-decoration: none;
+            color: var(--darkest);
+            height: 32px;
+            width: auto;
+        }
+
+        :root {
+            --lightest: whitesmoke;
+            --lighter: mintcream;
+            --light: azure;
+            --darkest: #031927;
+            --darker: #052e47;
+            --dark: #0a5c8f;
+        }
+
+        /* div::selection {
+            background: var(--darker);
+            color: var(--light);
+        } */
+
+        .center {
+            justify-content: center;
+            align-self: center;
+            align-items: center;
+            align-content: center;
+        }
+
+        /* .grid-container {} */
+
+        .flex {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .flex-wrap {
+            flex-wrap: wrap;
+        }
+
+        .main-container {
+            margin-top: 52px;
+            margin-bottom: 52px;
+        }
+
+        /* .grid-content {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: auto;
+            color: var(--dark);
+        } */
+
+        .card {
+            width: 300px;
+            background: var(--light);
+        }
+
+        .card-text {
+            color: var(--darkest);
+            text-align: center;
+            font-size: 2rem;
+        }
+
+        img {
+            width: 100%;
+        }
+
+        .border {
+            border: 12px;
+            border: medium solid var(--darker);
+        }
+
+        .border-bottom-fat {
+            border-bottom: 12px solid var(--dark);
+        }
+
+        .border-bottom {
+            border-bottom: 4px solid var(--light);
+        }
+
+        .border-radius {
+            border-radius: 12px;
+        }
+
+        .shadow {
+            box-shadow: 0 0 20px var(--darker);
+        }
+
+        /* padding margin  */
+        .margin-4 {
+            margin: 4px;
+        }
+
+        .margin-8 {
+            margin: 8px;
+        }
+
+        .margin-16 {
+            margin: 16px;
+        }
+
+        .padding-4 {
+            padding: 4px;
+        }
+
+        .padding-8 {
+            padding: 8px;
+        }
+
+        .padding-12 {
+            padding: 12px;
+        }
+
+        /*   */
+    </style>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
 </html>
