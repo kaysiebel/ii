394f0e22f8f70f320581ef57f248ddc624fb23fe
kay siebel <kay_siebel@gmx.de>
2020-05-04 20:13:56 +0200

    Switched to blade templates

---
+++ resources/views/home.blade.php
@@ -0,0 +1,159 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="main-container">
+    <div class="flex flex-wrap center">
+
+        <div class="card margin-16 border-radius shadow">
+            <img class="border-bottom-fat" src="https://www.citizencider.com/wp-content/uploads/2019/01/placeholder.jpg" alt="...">
+            <div class="card-content">
+                <p class="card-text padding-8 margin-8">
+                    Lorem
+                </p>
+            </div>
+        </div>
+
+        <div class="card margin-16 border-radius shadow">
+            <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320185/classicstaremblem-1682.png" alt="...">
+            <div class="card-content grid center">
+
+                <p class="card-text padding-8 margin-8">
+                    "Artikel-Name"</p>
+
+                <button class="accordion ripple border-bottom padding-4">Überblick</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Bestand: 1000 Stück</p>
+                    <p class="">
+                        Lager-Ort: Halle 3</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Gewicht</p>
+                    <p class="">
+                        Mindest-Bestand</p>
+                    <p class="">
+                        Bestell-Bestand</p>
+                    <p class="">
+                        Höchst-Bestand</p>
+                    <p class="">
+                        Gebinde-Größe</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Einheiten/Stück</p>
+                    <p class="">
+                        Bestand</p>
+                    <p class="">
+                        Gewicht</p>
+                    <p class="">
+                        Mindest-Bestand</p>
+                    <p class="">
+                        Bestell-Bestand</p>
+                    <p class="">
+                        Höchst-Bestand</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Lagerort</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Max-Gewicht</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">i!</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Gesamt-Gewicht/Stück</p>
+                    <p class="">
+                        Gesamt-Gewicht/Gebinde</p>
+                    <p class="">
+                        aktuelles Gewicht/Lagerort</p>
+                    <p class="">
+                        Letzter Zugang</p>
+                    <p class="">
+                        Letzter Abgang</p>
+                    <p class="">
+                        Durchschnittsbestand</p>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="card margin-16 border-radius shadow">
+            <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320195/tropicalhouse-1831.png" alt="...">
+            <div class="card-content grid center">
+
+                <p class="card-text padding-8 margin-8">
+                    "Artikel-Name"</p>
+
+                <button class="accordion ripple border-bottom padding-4">Überblick</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Bestand: 1000 Stück</p>
+                    <p class="">
+                        Lager-Ort: Halle 3</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Gewicht</p>
+                    <p class="">
+                        Mindest-Bestand</p>
+                    <p class="">
+                        Bestell-Bestand</p>
+                    <p class="">
+                        Höchst-Bestand</p>
+                    <p class="">
+                        Gebinde-Größe</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Einheiten/Stück</p>
+                    <p class="">
+                        Bestand</p>
+                    <p class="">
+                        Gewicht</p>
+                    <p class="">
+                        Mindest-Bestand</p>
+                    <p class="">
+                        Bestell-Bestand</p>
+                    <p class="">
+                        Höchst-Bestand</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">Lagerort</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Max-Gewicht</p>
+                </div>
+
+                <button class="accordion ripple border-bottom padding-4">i!</button>
+                <div class="accordion-panel border-bottom">
+                    <p class="">
+                        Gesamt-Gewicht/Stück</p>
+                    <p class="">
+                        Gesamt-Gewicht/Gebinde</p>
+                    <p class="">
+                        aktuelles Gewicht/Lagerort</p>
+                    <p class="">
+                        Letzter Zugang</p>
+                    <p class="">
+                        Letzter Abgang</p>
+                    <p class="">
+                        Durchschnittsbestand</p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</section>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,3 @@
+<section class="header-section border-radius">
+    <div class="header flex center">i!</div>
+</section>
