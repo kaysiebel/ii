0c5b5173d1633b4caa29f353b36cd04b9ca62fd7
kay siebel <kay_siebel@gmx.de>
2020-05-05 17:53:40 +0200

    finished create page and implemented in blade template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,84 +14,15 @@
         </div>
 
         <div class="card margin-16 border-radius shadow">
-            <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320185/classicstaremblem-1682.png" alt="...">
-            <div class="card-content grid center">
-
-                <p class="card-text padding-8 margin-8">
-                    "Artikel-Name"</p>
-
-                <button class="accordion ripple border-bottom padding-4">Überblick</button>
-                <div class="accordion-panel border-bottom">
-                    <p class="">
-                        Bestand: 1000 Stück</p>
-                    <p class="">
-                        Lager-Ort: Halle 3</p>
-                </div>
-
-                <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
-                <div class="accordion-panel border-bottom">
-                    <p class="">
-                        Gewicht</p>
-                    <p class="">
-                        Mindest-Bestand</p>
-                    <p class="">
-                        Bestell-Bestand</p>
-                    <p class="">
-                        Höchst-Bestand</p>
-                    <p class="">
-                        Gebinde-Größe</p>
-                </div>
-
-                <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
-                <div class="accordion-panel border-bottom">
-                    <p class="">
-                        Einheiten/Stück</p>
-                    <p class="">
-                        Bestand</p>
-                    <p class="">
-                        Gewicht</p>
-                    <p class="">
-                        Mindest-Bestand</p>
-                    <p class="">
-                        Bestell-Bestand</p>
-                    <p class="">
-                        Höchst-Bestand</p>
-                </div>
-
-                <button class="accordion ripple border-bottom padding-4">Lagerort</button>
-                <div class="accordion-panel border-bottom">
-                    <p class="">
-                        Max-Gewicht</p>
-                </div>
-
-                <button class="accordion ripple border-bottom padding-4">i!</button>
-                <div class="accordion-panel border-bottom">
-                    <p class="">
-                        Gesamt-Gewicht/Stück</p>
-                    <p class="">
-                        Gesamt-Gewicht/Gebinde</p>
-                    <p class="">
-                        aktuelles Gewicht/Lagerort</p>
-                    <p class="">
-                        Letzter Zugang</p>
-                    <p class="">
-                        Letzter Abgang</p>
-                    <p class="">
-                        Durchschnittsbestand</p>
-                </div>
-
-            </div>
-        </div>
-
-
-        <div class="card margin-16 border-radius shadow">
             <img class="border-bottom-fat" src="https://openclipart.org/image/800px/svg_to_png/320195/tropicalhouse-1831.png" alt="...">
             <div class="card-content grid center">
 
                 <p class="card-text padding-8 margin-8">
                     "Artikel-Name"</p>
 
-                <button class="accordion ripple border-bottom padding-4">Überblick</button>
+                <div class="accordion-styling accordion-top border-bottom padding-4 active"></div>
+
+                <div class="accordion accordion-styling ripple border-bottom padding-4">Überblick</div>
                 <div class="accordion-panel border-bottom">
                     <p class="">
                         Bestand: 1000 Stück</p>
@@ -99,7 +30,7 @@
                         Lager-Ort: Halle 3</p>
                 </div>
 
-                <button class="accordion ripple border-bottom padding-4">Stück-Angaben</button>
+                <div class="accordion accordion-styling ripple border-bottom padding-4">Stück-Angaben</div>
                 <div class="accordion-panel border-bottom">
                     <p class="">
                         Gewicht</p>
@@ -113,7 +44,7 @@
                         Gebinde-Größe</p>
                 </div>
 
-                <button class="accordion ripple border-bottom padding-4">Gebinde-Angaben</button>
+                <div class="accordion accordion-styling ripple border-bottom padding-4">Gebinde-Angaben</div>
                 <div class="accordion-panel border-bottom">
                     <p class="">
                         Einheiten/Stück</p>
@@ -129,13 +60,13 @@
                         Höchst-Bestand</p>
                 </div>
 
-                <button class="accordion ripple border-bottom padding-4">Lagerort</button>
+                <div class="accordion accordion-styling ripple border-bottom padding-4">Lagerort</div>
                 <div class="accordion-panel border-bottom">
                     <p class="">
                         Max-Gewicht</p>
                 </div>
 
-                <button class="accordion ripple border-bottom padding-4">i!</button>
+                <div class="accordion accordion-styling ripple border-bottom padding-4">i!</div>
                 <div class="accordion-panel border-bottom">
                     <p class="">
                         Gesamt-Gewicht/Stück</p>
@@ -150,6 +81,8 @@
                     <p class="">
                         Durchschnittsbestand</p>
                 </div>
+
+                <div class="accordion-styling accordion-bottom border-bottom padding-4"></div>
 
             </div>
         </div>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -15,17 +15,17 @@
                     <div class="accordion accordion-styling ripple border-bottom padding-4">Artikel-Daten</div>
                     <div class="accordion-panel border-bottom" style="display: block;">
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Artikel-Name">Name</label>
                             <input class="create-input" type="text" name="Artikel-Name" placeholder="erforderlich">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Anfangs-Bestand">Anfangs- Bestand</label>
                             <input class="create-input width-small" type="text" name="Anfangs-Bestand" placeholder="erforderlich">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Lagerplatz">Lagerplatz</label>
                             <input class="create-input" type="text" name="Lagerplatz" placeholder="verzichtbar">
                         </div>
@@ -34,27 +34,27 @@
                     <div class="accordion accordion-styling ripple border-bottom padding-4">Stück- Angaben</div>
                     <div class="accordion-panel border-bottom">
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Mindest-Bestand">Mindest- Bestand</label>
                             <input class="create-input width-small" type="text" name="Mindest-Bestand" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Bestell-Bestand">Bestell- Bestand</label>
                             <input class="create-input width-small" type="text" name="Bestell-Bestand" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Höchst-Bestand">Höchst- Bestand</label>
                             <input class="create-input width-small" type="text" name="Höchst-Bestand" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Gebinde-Größe">Gebinde- Größe</label>
                             <input class="create-input width-xsmall" type="text" name="Gebinde-Größe" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Gewicht">Gewicht</label>
                             <input class="create-input width-xsmall" type="text" name="Gewicht" placeholder="verzichtbar">
                         </div>
@@ -64,29 +64,29 @@
                     <div class="accordion accordion-styling ripple border-bottom padding-4">Gebinde-Angaben</div>
                     <div class="accordion-panel border-bottom">
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Gebinde-Einheit">Gebinde- Einheit</label>
                             <input class="create-input width-xsmall" type="text" name="Gebinde-Einheit" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Bestand">Bestand</label>
                             <input class="create-input width-small" type="text" name="Bestand" placeholder="verzichtbar">
                         </div>
 
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Gewicht">Gewicht</label>
                             <input class="create-input width-xsmall" type="text" name="Gewicht" placeholder="verzichtbar">
                         </div>
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Mindest-Bestand">Mindest- Bestand</label>
                             <input class="create-input width-small" type="text" name="Mindest-Bestand" placeholder="verzichtbar">
                         </div>
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Bestell-Bestand">Bestell- Bestand</label>
                             <input class="create-input width-small" type="text" name="Bestell-Bestand" placeholder="verzichtbar">
                         </div>
-                        <div class="input-large grid-2 center">
+                        <div class="input grid-2 center">
                             <label class="create-label" for="Höchst-Bestand">Höchst- Bestand</label>
                             <input class="create-input width-small" type="text" name="Höchst-Bestand" placeholder="verzichtbar">
                         </div>
@@ -104,57 +104,4 @@
     </div>
 </section>
 
-<style>
-    .card-create {
-        min-height: 500px;
-        background: var(--light);
-        color: var(--dark);
-        font-family: 'roboto', sans-serif;
-    }
-
-    .create-head {
-        font-size: rem;
-    }
-
-    .create-content {
-        padding: 12px 12px 0 12px;
-    }
-
-    .create-label {
-        font-size: 18px;
-        text-align: right;
-        margin-right: 20px;
-        /* padding-left: 20px; */
-    }
-
-    .create-input {
-        height: 28px;
-        border: 2px solid var(--dark);
-    }
-
-
-    .input-large {
-        /* text-align: left; */
-        padding: 12px;
-        border: 1px solid var(--lightest);
-    }
-
-    .grid-2 {
-        display: grid;
-        grid-template-columns: 100px 1fr;
-        grid-auto-rows: 32px;
-    }
-
-    .width-small {
-        width: 100px;
-    }
-
-    .width-xsmall {
-        width: 40px;
-    }
-
-    .left {
-        justify-content: left;
-    }
-</style>
 @endsection
