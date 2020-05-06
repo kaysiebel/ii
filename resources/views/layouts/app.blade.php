d3954dde7bbc2dd036661e939a3ff1091e695feb
kay siebel <kay_siebel@gmx.de>
2020-05-06 15:34:25 +0200

    implemented suggested improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </link> -->
-    <link rel="stylesheet" href="http://ii.test/css/app.css">
-    <!-- <link rel="stylesheet" href="/css/app.css"> -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     <title>i!</title>
 </head>
 
