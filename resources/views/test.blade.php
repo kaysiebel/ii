c2120ae5def2267b6cb0b887b183f33ae911cdea
kay siebel <kay_siebel@gmx.de>
2020-05-10 12:23:13 +0200

    Last changes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,32 +1,72 @@
-@extends('layouts.app')
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-<section class="page-container">
 
-    <div class="bar-container">
-        <div class="bar-grid acc ripple">
-            <img class="bar-img" src="https://picsum.photos/200/200 " alt="...">
-            <div class="bar-header">
-                <p class="bar-title">Name des Artikels</p>
-                <p class="bar-text">Soll- Bestand: 20.000 Stück</p>
-            </div>
-            <div class="dot-container">
-                <div class="bar-dot">&nbsp;</div>
-            </div>
-        </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
-        <div id="main-accordion" class="main-accordion panel" style="display: none;">
-            @include('includes/sections/home/overview')
-            @include('includes/sections/home/piece')
-            @include('includes/sections/home/unit')
-            @include('includes/sections/home/location')
-            @include('includes/sections/home/entry')
-            @include('includes/sections/home/magic')
-            @include('includes/sections/home/edit')
-        </div>
-    </div>
+<body>
+    <canvas id="myChart" width="400" height="400"></canvas>
+    <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var mixedChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: 'Bestand',
+                    data: [12000, 10700, 10700, 9400, 8400, 10400, 8700, 9000, ],
 
-</section>
+                    // Changes this dataset to become a line
+                    type: 'line',
+                    backgroundColor: 'rgba(10, 92, 143, 0.6',
+                    borderColor: 'rgba(10, 92, 143, 1)',
+                    borderWidth: 1
+                }, {
+                    label: 'Verbrauch',
+                    data: [1000, 1300, 2000, 1300, 1000, 2000, 1700, 1300, ],
 
-@endsection
+                    // Changes this dataset to become a line
+                    type: 'bar',
+                    backgroundColor: 'rgba(220, 0, 0, 0.6',
+                    borderColor: 'rgba(220, 0, 0, 1)',
+                    borderWidth: 1
+                }, {
+                    label: 'Eingänge',
+                    data: [4000, 0, 2000, 0, 0, 4000, 0, 1000, ],
+                    backgroundColor: 'rgba(150, 188, 58, 0.6',
+                    borderColor: 'rgba(150, 188, 58, 1)',
+                    borderWidth: 1
+                }],
+                labels: ["01.05.", "02.05.", "03.05", "04.05", "05.05", "6.05.", "7.05.", "8.05.", ]
+            },
+
+            // var myChart = new Chart(ctx, {
+            //     type: 'bar',
+            //     data: {
+            //         labels: ["01.05.", "02.05.", "04.05", "05.05", "08.05", "10.05."],
+            //         datasets: [{
+            //             label: 'Eingänge',
+            // data: [4000, 2000, 4000, 1000, 4000, 2000],
+            // backgroundColor: 'rgba(150, 188, 58, 0.6',
+            // borderColor: 'rgba(150, 188, 58, 1)',
+            // borderWidth: 1
+            //         }]
+            //     },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+</body>
+
+</html>
