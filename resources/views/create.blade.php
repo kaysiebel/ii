700b0aba883a12fbbebc108fd6e5191db17602cf
kay siebel <kay_siebel@gmx.de>
2020-05-11 20:35:50 +0200

    Working on create page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,23 +2,19 @@
 
 @section('content')
 
-<section class="page-container">
-
-    <div class="bar-container">
-        <div class="bar-grid-create acc ripple">
-            <img class="bar-img" src="https://openclipart.org/image/800px/svg_to_png/320195/tropicalhouse-1831.png" alt="...">
-            <div class="bar-header">
-                <p class="bar-title">Hier können neue Artikel angelegt werden</p>
-            </div>
-        </div>
-
-        <div class="main-accordion panel" style="display: block;">
-            @include('includes/sections/create/articledata')
-            @include('includes/sections/create/piecedata')
-            @include('includes/sections/create/unitdata')
+<div class="bar-container">
+    <div class="bar-grid-create acc ripple">
+        <img class="bar-img" src="https://openclipart.org/image/800px/svg_to_png/320195/tropicalhouse-1831.png" alt="...">
+        <div class="bar-header">
+            <p class="bar-title">Hier können neue Artikel angelegt werden</p>
         </div>
     </div>
 
-</section>
+    <div class="main-accordion panel" style="display: block;">
+        @include('includes/sections/create/articledata')
+        @include('includes/sections/create/piecedata')
+        @include('includes/sections/create/unitdata')
+    </div>
+</div>
 
 @endsection
