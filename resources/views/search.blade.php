3236065b4440cec82f39d6609567e819dd220dd8
kay siebel <kay_siebel@gmx.de>
2020-06-01 15:32:20 +0200

    Fixed styling bug on search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -21,34 +21,4 @@
 
 </div>
 
-@foreach($articles as $article)
-
-<div class="bar-container">
-
-    <div class="bar-grid acc ripple">
-        <div>
-            <a href="{{ url("/articles/{$article->id}") }}">
-                <img class="bar-img" src="{{ $article->imageUrl() }}" alt="...">
-            </a>
-        </div>
-        <div class="bar-header">
-            <p class="bar-title">{{ $article->name }}</p>
-            <p class="bar-text">Ist- Bestand: {{ $article->piece_start_stock }} Stk</p>
-        </div>
-
-        <div class="dot-container">
-            <div class="bar-dot">&nbsp;</div>
-        </div>
-
-    </div>
-
-    <div class="main-accordion none">
-        @include('includes/sections/home/overview')
-        @include('includes/sections/home/entry')
-    </div>
-
-</div>
-
-@endforeach
-
 @endsection
--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
+<div class="bar-container">
+
+    <div class="bar-full ripple active">
+        <div class="bar-header">
+            <p class="bar-title">Artikel- Suche</p>
+
+        </div>
+    </div>
+
+    <div class="accordion-panel block">
+        <form class="mininav margin-8" method="GET" action="{{ url('/searchresult') }}">
+            @csrf
+            <input type="text" class="create-input" placeholder="Artikel Name" name="q">
+            <button class="search-btn ripple" type="submit"><i class="">los</i></button>
+        </form>
+    </div>
+
+</div>
+
 @foreach($articles as $article)
 
 <div class="bar-container">
