7d26f3cbb91696ed74936fb5189f4bd58dc4c8d1
kay siebel <kay_siebel@gmx.de>
2020-06-02 12:58:09 +0200

    Additional readjustment

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>i!</title>
-    <meta http-equiv="refresh" content="2.8; url=/articles" />
+    <meta http-equiv="refresh" content="3; url=/articles" />
 </head>
 
 <body>
